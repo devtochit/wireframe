207a256687075c6c432aa41cfa985401b1c511f4
officialesse <uzomaesse@gmail.com>
2022-05-23 17:50:27 +0100

    added javascript

---
+++ script/index.js
@@ -0,0 +1,16 @@
+let title = document.getElementById("title")
+let name  = document.getElementById("name")
+let bookTitle= document.querySelector(".author-book-title")
+let authorName = document.querySelector(".author-name")
+
+
+
+
+
+function save (title,name){
+    title.value="textValue";
+    name.value="textValue";
+    bookTitle.innerHTML= "title"
+
+
+}
