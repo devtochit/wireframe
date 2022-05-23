23a9d13db956d3261645ed6fd94ef15a10336728
jesse uzoma <71229278+officailesse@users.noreply.github.com>
2022-05-23 13:23:24 -0600

    added the remove function

--- script/index.js
+++ script/index.js
@@ -1,9 +1,13 @@
-const books = [{
-    title : "ABC Murders",
-    author : "Agatha Christie",
+
+
+
+
+let books = [{
+    title: "ABC Murders",
+    author: "Agatha Christie",
 }, {
-    title : "Origin",
-    author : "Dan Brown",
+    title: "Origin",
+    author: "Dan Brown",
 }];
 
 function book(title, author) {
@@ -14,9 +18,20 @@ function book(title, author) {
 function add_book(title, author) {
     const book1 = new book(title, author);
     books.push(book1);
+
 }
+function removebook(title, author) {
+    books = books.filter(book => {
+        if (book.title != title && book.author != author)
+            return true
+
+    });
+}
+
+removebook("ABC Murders", "Agatha Christie")
 
 add_book("Name of the book", "Author of the book");
 add_book("Name of the second book", "Author of the second book");
 console.log(books.length);
-console.log(books[3].title);
+//console.log(books[3].title);
+removebook("Name of the book", "Author of the book");
