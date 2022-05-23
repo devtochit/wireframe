32cf0bcc7df0a8d5ef951291072db92c64b4f9e0
Elson Otake <elsonotake@yahoo.com.br>
2022-05-23 14:45:29 -0300

    Add function to add books

--- script/index.js
+++ script/index.js
@@ -5,3 +5,18 @@ const books = [{
     title : "Origin",
     author : "Dan Brown",
 }];
+
+function book(title, author) {
+    this.title = title;
+    this.author = author;
+}
+
+function add_book(title, author) {
+    const book1 = new book(title, author);
+    books.push(book1);
+}
+
+add_book("Name of the book", "Author of the book");
+add_book("Name of the second book", "Author of the second book");
+console.log(books.length);
+console.log(books[3].title);
