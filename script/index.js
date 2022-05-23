8d8a9fa7221a99e15137e2fcb39620fc24c16ee8
Elson Otake <elsonotake@yahoo.com.br>
2022-05-23 14:34:55 -0300

    Add list of books

--- script/index.js
+++ script/index.js
@@ -1,16 +1,7 @@
-let title = document.getElementById("title")
-let name  = document.getElementById("name")
-let bookTitle= document.querySelector(".author-book-title")
-let authorName = document.querySelector(".author-name")
-
-
-
-
-
-function save (title,name){
-    title.value="textValue";
-    name.value="textValue";
-    bookTitle.innerHTML= "title"
-
-
-}
+const books = [{
+    title : "ABC Murders",
+    author : "Agatha Christie",
+}, {
+    title : "Origin",
+    author : "Dan Brown",
+}];
